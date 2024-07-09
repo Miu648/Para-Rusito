491a22f88e1ac6e80aa0b3909f2ccf08e80d4fc1
Miu648 <baezmia484@gmail.com>
2024-07-09 03:03:03 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+setTimeout(()=>{
+    document.querySelector('#loading-message')
+    .innerText = '♥ ¡Enviado! ♥'
+setTimeout(()=>alert('¡Recibiste un abrazo virtual! Te quiero muchísimo, ojalá tengas un muy buen día y unos bellos 16 añitos. - Miu.'))
+},4200)
